1c0bfb65600f645b3c0da1997cf876b55ac1f9b3
Chapman Shoop <chapman.shoop@gmail.com>
2025-08-28 19:57:47 -0500

    Let's try a smaller terrain map to start with. Escape key to quit, Q and E to control altitude.

--- addons/proto_controller/proto_controller.gd
+++ addons/proto_controller/proto_controller.gd
@@ -80,7 +80,14 @@ func _physics_process(delta: float) -> void:
 	# If freeflying, handle freefly and nothing else
 	if can_freefly and freeflying:
 		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
-		var motion := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		var z_axis_dir = 0
+
+		if Input.is_action_pressed("fly_up"):
+			z_axis_dir = 1
+		elif Input.is_action_pressed("fly_down"):
+			z_axis_dir = -1
+
+		var motion := (head.global_basis * Vector3(input_dir.x, z_axis_dir, input_dir.y)).normalized()
 		motion *= freefly_speed * delta
 		move_and_collide(motion)
 		return
--- temple_texas.gd
+++ temple_texas.gd
@@ -2,7 +2,14 @@ extends Node3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	update_minimap()
+
+func update_minimap() -> void:
 	var drone_pos = %TourismDrone.global_position
-	drone_pos = drone_pos * (2560.0 / 4096.0)
+	drone_pos = drone_pos * (2560.0 / 1081.0)
 	%MiniMap.texture.region.position.x = drone_pos.x - 200
 	%MiniMap.texture.region.position.y = drone_pos.z - 200
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
