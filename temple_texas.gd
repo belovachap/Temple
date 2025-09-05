6f509a858e01b863b215adb222d2ef8f9fb9e888
Chapman Shoop <chapman.shoop@gmail.com>
2025-09-04 22:41:20 -0500

    Just moving and sliding the drone around :)

--- temple_texas.gd
+++ temple_texas.gd
@@ -3,6 +3,7 @@ extends Node3D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	update_minimap()
+	update_follow_camera()
 
 func update_minimap() -> void:
 	var drone_pos = %TourismDrone.global_position
@@ -10,6 +11,21 @@ func update_minimap() -> void:
 	%MiniMap.texture.region.position.x = drone_pos.x - 200
 	%MiniMap.texture.region.position.y = drone_pos.z - 200
 
+func update_follow_camera() -> void:
+	var boop = %TourismDrone.basis.x.normalized() * 10
+	%FollowCamera3D.global_position.x = %TourismDrone.global_position.x - boop.x
+	%FollowCamera3D.global_position.y = %TourismDrone.global_position.y + 10
+	%FollowCamera3D.global_position.z = %TourismDrone.global_position.z - boop.z
+	%FollowCamera3D.look_at(%TourismDrone.global_position)
+
 func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("switch_cameras"):
+		if %FollowCamera3D.is_current():
+			%FollowCamera3D.clear_current()
+			%TourismDrone.get_node("%Camera3D").make_current()
+		else:
+			%TourismDrone.get_node("%Camera3D").clear_current()
+			%FollowCamera3D.make_current()
+
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
--- tourism_drone.gd
+++ tourism_drone.gd
@@ -1,133 +1,47 @@
-extends RigidBody3D
+extends CharacterBody3D
 
-const MAX_ENGINE_POWER = 0.8
-const MIN_ENGINE_POWER = 0.0
-const POWER_ADJUSTMENT_Y_PER_SECOND = MAX_ENGINE_POWER * 10
-const POWER_ADJUSTMENT_X_PER_SECOND = MAX_ENGINE_POWER * 10
+const MAX_VELOCITY = 10.0 # m/s
+const ACCELERATION = MAX_VELOCITY # m/s^2
 
-var engine1_power = MAX_ENGINE_POWER
-var engine2_power = MAX_ENGINE_POWER
-var engine3_power = MAX_ENGINE_POWER
-var engine4_power = MAX_ENGINE_POWER
-
-var desired_y_velocity = 0.0
-var y_pos_before
-var y_pos_now
-var y_velocity
-
-var desired_x_velocity = 0.0
-var x_pos_before
-var x_pos_now
-var x_velocity
-
-func _ready() -> void:
-	y_pos_now = global_position.y
-	x_pos_now = global_position.x
-
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("drone_ascend"):
-		desired_y_velocity = 1.0
-	elif Input.is_action_pressed("drone_descend"):
-		desired_y_velocity = -1.0
-	else:
-		desired_y_velocity = 0.0
-
-	if Input.is_action_pressed("drone_forward"):
-		desired_x_velocity = 0.1
-	elif Input.is_action_pressed("drone_backward"):
-		desired_x_velocity = -0.1
-	else:
-		desired_x_velocity = 0.0
+const MAX_ROTATION = PI / 8 # degrees/s
+const ANGULAR_ACCELERATION = MAX_ROTATION # degrees/s^2
 
 func _physics_process(delta: float) -> void:
-	y_pos_before = y_pos_now
-	y_pos_now = global_position.y
-	y_velocity = (y_pos_now - y_pos_before) / delta
-
-	x_pos_before = x_pos_now
-	x_pos_now = global_position.x
-	x_velocity = (x_pos_now - x_pos_before) / delta
-
-	var euler = global_transform.basis.get_euler() # radians
-	var roll_deg = rad_to_deg(euler.x)
-	var yaw_deg = rad_to_deg(euler.y)
-	var pitch_deg = rad_to_deg(euler.z)
-
-	if y_velocity < desired_y_velocity:
-		engine1_power += POWER_ADJUSTMENT_Y_PER_SECOND * delta
-		engine2_power += POWER_ADJUSTMENT_Y_PER_SECOND * delta
-		engine3_power += POWER_ADJUSTMENT_Y_PER_SECOND * delta
-		engine4_power += POWER_ADJUSTMENT_Y_PER_SECOND * delta
+	if Input.is_action_pressed("drone_ascend"):
+		velocity.y += ACCELERATION * delta
+	elif Input.is_action_pressed("drone_descend"):
+		velocity.y -= ACCELERATION * delta
 	else:
-		engine1_power -= POWER_ADJUSTMENT_Y_PER_SECOND * delta
-		engine2_power -= POWER_ADJUSTMENT_Y_PER_SECOND * delta
-		engine3_power -= POWER_ADJUSTMENT_Y_PER_SECOND * delta
-		engine4_power -= POWER_ADJUSTMENT_Y_PER_SECOND * delta
+		if velocity.y > 0.0:
+			velocity.y -= ACCELERATION * delta
+		elif velocity.y < 0.0:
+			velocity.y += ACCELERATION * delta
 
-	engine1_power = clampf(engine1_power, MIN_ENGINE_POWER, MAX_ENGINE_POWER)
-	engine2_power = clampf(engine2_power, MIN_ENGINE_POWER, MAX_ENGINE_POWER)
-	engine3_power = clampf(engine3_power, MIN_ENGINE_POWER, MAX_ENGINE_POWER)
-	engine4_power = clampf(engine4_power, MIN_ENGINE_POWER, MAX_ENGINE_POWER)
-
-	var engine1_adjusted = engine1_power
-	var engine2_adjusted = engine2_power
-	var engine3_adjusted = engine3_power
-	var engine4_adjusted = engine4_power
-
-	
-	const PITCH_ADJUSTMENT = 0.001
-	if desired_x_velocity > 0:
-		if pitch_deg > -0.05:
-			engine1_adjusted -= PITCH_ADJUSTMENT
-			engine2_adjusted -= PITCH_ADJUSTMENT
-			engine3_adjusted += PITCH_ADJUSTMENT
-			engine4_adjusted += PITCH_ADJUSTMENT
-	elif desired_x_velocity < 0:
-		if pitch_deg < 0.05:
-			engine1_adjusted += PITCH_ADJUSTMENT * 2
-			engine2_adjusted += PITCH_ADJUSTMENT * 2
-			engine3_adjusted -= PITCH_ADJUSTMENT * 2
-			engine4_adjusted -= PITCH_ADJUSTMENT * 2
+	if Input.is_action_pressed("drone_forward"):
+		velocity += basis.x * ACCELERATION * delta
+	elif Input.is_action_pressed("drone_backward"):
+		velocity -= basis.x * ACCELERATION * delta
 	else:
-		if pitch_deg > 0:
-			engine1_adjusted -= PITCH_ADJUSTMENT * 2
-			engine2_adjusted -= PITCH_ADJUSTMENT * 2
-			engine3_adjusted += PITCH_ADJUSTMENT * 2
-			engine4_adjusted += PITCH_ADJUSTMENT * 2
-		else:
-			engine1_adjusted += PITCH_ADJUSTMENT * 2
-			engine2_adjusted += PITCH_ADJUSTMENT * 2
-			engine3_adjusted -= PITCH_ADJUSTMENT * 2
-			engine4_adjusted -= PITCH_ADJUSTMENT * 2
+		if basis.x.dot(velocity) > 0.1:
+			velocity -= basis.x * ACCELERATION * delta
+		elif basis.x.dot(velocity) < 0.0:
+			velocity += basis.x * ACCELERATION * delta
 
-	engine1_adjusted = clampf(engine1_adjusted, MIN_ENGINE_POWER, MAX_ENGINE_POWER)
-	engine2_adjusted = clampf(engine2_adjusted, MIN_ENGINE_POWER, MAX_ENGINE_POWER)
-	engine3_adjusted = clampf(engine3_adjusted, MIN_ENGINE_POWER, MAX_ENGINE_POWER)
-	engine4_adjusted = clampf(engine4_adjusted, MIN_ENGINE_POWER, MAX_ENGINE_POWER)
+	if Input.is_action_pressed("drone_roll_left"):
+		velocity -= basis.z * ACCELERATION * delta
+	elif Input.is_action_pressed("drone_roll_right"):
+		velocity += basis.z * ACCELERATION * delta
+	else:
+		if basis.z.dot(velocity) > 0.0:
+			velocity -= basis.z * ACCELERATION * delta
+		elif basis.z.dot(velocity) < 0.0:
+			velocity += basis.z * ACCELERATION * delta
 
-	var engine1_y_basis = %Engine1.global_transform.basis.orthonormalized().y
-	var engine2_y_basis = %Engine2.global_transform.basis.orthonormalized().y
-	var engine3_y_basis = %Engine3.global_transform.basis.orthonormalized().y
-	var engine4_y_basis = %Engine4.global_transform.basis.orthonormalized().y
-	
-	apply_force(engine1_y_basis * engine1_adjusted, %Engine1.position)
-	apply_force(engine2_y_basis * engine2_adjusted, %Engine2.position)
-	apply_force(engine3_y_basis * engine3_adjusted, %Engine3.position)
-	apply_force(engine4_y_basis * engine4_adjusted, %Engine4.position)
+	if Input.is_action_pressed("drone_rotate_left"):
+		rotate(Vector3(0.0, 1.0, 0.0), ANGULAR_ACCELERATION * delta)
+	elif Input.is_action_pressed("drone_rotate_right"):
+		rotate(Vector3(0.0, 1.0, 0.0), -ANGULAR_ACCELERATION * delta)
 
-func _on_debug_timer_timeout() -> void:
-	print("Position: ", global_position)
-	print("Y Velocity: ", y_velocity)
-	print("X Velocity: ", x_velocity)
-	print("Engine 1 Power: ", engine1_power)
-	print("Engine 2 Power: ", engine2_power)
-	print("Engine 3 Power: ", engine3_power)
-	print("Engine 4 Power: ", engine4_power)
-	
-	var euler = global_transform.basis.get_euler() # radians
-	var roll_deg = rad_to_deg(euler.x)
-	var yaw_deg = rad_to_deg(euler.y)
-	var pitch_deg = rad_to_deg(euler.z)
-	print("roll_deg: ", roll_deg)
-	print("yaw_deg: ", yaw_deg)
-	print("pitch_deg: ", pitch_deg)
+	velocity = velocity.limit_length(MAX_VELOCITY)
+
+	move_and_slide()
